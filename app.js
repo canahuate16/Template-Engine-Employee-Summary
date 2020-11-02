5902fea562b95496bd535abfc4e3fcb3921bf495
Luis Canahuate <luis.canahuate16@gmail.com>
2020-11-01 19:22:04 -0500

    fixed paths

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./Develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
+const render = require("./Develop/lib/htmlRenderer");
+const Employee = require("./Develop/lib/Employee");
 const util = require("util");
 util.promisify(fs.writeFile);
 
