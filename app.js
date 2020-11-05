3c527d3505d823eae3960d2ae14995ddba656d9c
Luis Canahuate <luis.canahuate16@gmail.com>
2020-11-05 18:01:59 -0500

    updated writeHTML function to include Render function

--- app.js
+++ app.js
@@ -8,34 +8,8 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 const util = require("util");
 util.promisify(fs.writeFile);
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 let teamMembers = [];
 
@@ -69,7 +43,7 @@ function start() {
     ])
 
         .then((managerData) => {
-           
+
             const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
             teamMembers.push(manager);
             console.log(managerData);
@@ -105,11 +79,12 @@ function selectMember() {
                 start()
 
             } else {
-                render(teamMembers)
+                writeHTML();
             }
 
         })
 }
+//Engineer questions
 function createEngineer() {
     inquirer.prompt([
         {
@@ -138,15 +113,12 @@ function createEngineer() {
         .then((data) => {
             console.log(data)
             const newEmployee = new Engineer(data.name, data.id, data.email, data.github);
-            // teamMember.push (employee)
-            //  console.log(employees);
             teamMembers.push(newEmployee)
             console.log(teamMembers);
-            // makeTeam()
             selectMember()
         })
 }
-
+//intern questions 
 function createIntern() {
     inquirer.prompt([
         {
@@ -169,8 +141,6 @@ function createIntern() {
             name: "school",
             message: "What is your intern's school?"
         }
-
-
     ])
         .then((data) => {
             const addIntern = new Intern(data.name, data.id, data.email, data.school);
@@ -181,13 +151,13 @@ function createIntern() {
         })
 }
 
-
-
-// function makeTeam() {
-//     if (!fs.existsSync(OUTPUT_DIR)) {
-//         fs.mkdirSync(OUTPUT_DIR)
-//       }
-//  }
-//  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+function writeHTML() {
+    fs.writeFile("index.html", render(teamMembers), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!")
+    })
+}
 
 start();
